03e68e8d602d92c7b268068255b42c6dbf1b5299
Riski Afendi <riskistar2703@gmail.com>
2024-09-03 14:26:33 +0700

    add literalKonstan.kt

---
+++ src/main/kotlin/literalKonstan.kt
@@ -0,0 +1,18 @@
+fun main(args: Array<String>) {
+    val floatLiteral: Float = 178.95F //Literal Float
+    println("Contoh Literal Float : " + floatLiteral)
+
+    val oneBillion: Long = 1000000000L //Lieteral Long
+    println("Contoh literal Long : " + oneBillion)
+
+    val valHeksa = 0x0f //Lieral heksadesimal diawali dengan 0x
+    println("Contoh literal Heksadesimal : " + valHeksa)
+
+    val valBinary = 0b00001011 //Lieral binary diawali dengan 0b
+    println("Contoh Lieral Binary : " + valBinary)
+
+    val notasiKonvensional = 123.5e10
+    println("Contoh literal Konvensional Floating Floating Point Number : " + notasiKonvensional)
+
+
+}
