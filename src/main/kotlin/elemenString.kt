fafe749be41c3260e991ab66daa03d3071756eb4
Riski Afendi <riskistar2703@gmail.com>
2024-09-06 19:38:12 +0700

    add elemenString.kt

---
+++ src/main/kotlin/elemenString.kt
@@ -0,0 +1,15 @@
+
+//contoh kode program mengakses eleme string
+fun main(args: Array<String>) {
+
+    val myName: String = "Malaikat Israfil"
+    for (chr in myName) {
+        print(chr)
+    }
+    print('\n')
+
+}
+
+
+
+
