c8210721009704d2028e86255780b459af67dbaf
Riski Afendi <riskistar2703@gmail.com>
2024-09-06 19:26:11 +0700

    add boolean.kt

---
+++ src/main/kotlin/boolean.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+
+    var varBool : Boolean = true
+    if (varBool)
+        println("varBool bernilai " + varBool)
+
+    var  varInt : Int = 1
+    if (varInt !=0 ) //memeriksa jika varInt bukan nol
+        println("varInt bernilai " + varInt)
+}
