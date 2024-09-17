e4920dfcedb32853a96f620e34414567c7b3ac66
Riski Afendi <riskistar2703@gmail.com>
2024-09-17 08:35:05 +0700

    add underscoreLiteral.kt

--- src/main/kotlin/underscoreLiteral.kt
+++ src/main/kotlin/underscoreLiteral.kt
@@ -3,7 +3,7 @@ fun main(args: Array<String>) {
     val creditCardNumber = 1234_5678_9012_3456L
     val socialSecurityNumber = 999_99_9999L
     val hexBytes = 0xFF_EC_DE_5E
-    val bytes = 0b11010010_01101001_10010100_10010010
+    val bytes = 0b11010010_01101001_10010100_10010010  //32 bit
 
     println("oneMillion = " + oneMillion)
     println("creditCardNumber = " + creditCardNumber)
